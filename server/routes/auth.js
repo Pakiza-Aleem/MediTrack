ca49c5762cac9ebe5b31c6251e66c80787d434b8
Pakiza-Aleem <pakeezaaleem@gmail.com>
2026-09-05 19:42:17 -0300

    updated auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -18,8 +18,8 @@ const router = express.Router();
  */
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
 };
 
